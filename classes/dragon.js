62ca016fc99d0e9b2c4bee960d90eb18dbc32b29
danehoov <DaneHoov22@gmail.com>
2024-04-25 16:14:59 -0600

    dragon solved

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,20 @@
-// Your code here 
+class Dragon {
+
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+  static getDragons(...dragons) {
+    return dragons
+    .filter(dragon => dragon instanceof Dragon)
+    .map(dragon => dragon.name)
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
